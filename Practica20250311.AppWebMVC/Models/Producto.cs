da026969d44f51e53f17de4079a120061bb46998
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-03-11 09:06:53 -0600

    Finalizacion de los combos

--- Practica20250311.AppWebMVC/Models/Producto.cs
+++ Practica20250311.AppWebMVC/Models/Producto.cs
@@ -8,10 +8,13 @@ public partial class Producto
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
 
     public string? Descripcion { get; set; }
 
+    [Required(ErrorMessage = "El precio es obligatorio.")]
+    [Range(typeof(decimal), "0.01", "99999999.99", ErrorMessage = "El precio debe estar entre 0.01 y 99999999.99")]
     public decimal Precio { get; set; }
 
     [Display(Name = "Marcas")]
